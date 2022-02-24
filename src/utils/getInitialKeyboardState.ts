e84f3b45eb72f6840d362cc44a632b2fa0c126c1
Henry Seongwook Ha <swh0901@gmail.com>
2022-02-25 02:04:19 +0900

    fix getInitialKeyboardState build error

--- src/utils/getInitialKeyboardState.ts
+++ src/utils/getInitialKeyboardState.ts
@@ -2,9 +2,37 @@ import { KeyboardState, KEYS } from 'constants/keyboard'
 import { KeyboardButton } from 'types/keyboard'
 
 export const getInitialKeyboardState = () => {
-  const keyboardState: KeyboardButton = {}
-
-  KEYS.forEach((key) => (keyboardState[key] = KeyboardState.NOT_PRESSED))
+  const keyboardState: KeyboardButton = {
+    '': KeyboardState.NOT_PRESSED,
+    Q: KeyboardState.NOT_PRESSED,
+    W: KeyboardState.NOT_PRESSED,
+    E: KeyboardState.NOT_PRESSED,
+    R: KeyboardState.NOT_PRESSED,
+    T: KeyboardState.NOT_PRESSED,
+    Y: KeyboardState.NOT_PRESSED,
+    U: KeyboardState.NOT_PRESSED,
+    I: KeyboardState.NOT_PRESSED,
+    O: KeyboardState.NOT_PRESSED,
+    P: KeyboardState.NOT_PRESSED,
+    A: KeyboardState.NOT_PRESSED,
+    S: KeyboardState.NOT_PRESSED,
+    D: KeyboardState.NOT_PRESSED,
+    F: KeyboardState.NOT_PRESSED,
+    G: KeyboardState.NOT_PRESSED,
+    H: KeyboardState.NOT_PRESSED,
+    J: KeyboardState.NOT_PRESSED,
+    K: KeyboardState.NOT_PRESSED,
+    L: KeyboardState.NOT_PRESSED,
+    ENTER: KeyboardState.NOT_PRESSED,
+    Z: KeyboardState.NOT_PRESSED,
+    X: KeyboardState.NOT_PRESSED,
+    C: KeyboardState.NOT_PRESSED,
+    V: KeyboardState.NOT_PRESSED,
+    B: KeyboardState.NOT_PRESSED,
+    N: KeyboardState.NOT_PRESSED,
+    M: KeyboardState.NOT_PRESSED,
+    DEL: KeyboardState.NOT_PRESSED,
+  }
 
   return keyboardState
 }
